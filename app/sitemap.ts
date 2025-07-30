f625ddb382f52951054d1e96fff1d69fe48b88c3
Utkarsh Singh <69565691+realutkarshh@users.noreply.github.com>
2025-07-30 18:05:40 +0530

    Fixed SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,31 +3,31 @@ import type { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://skmpublicschool.edu",
+      url: "https://skmps.org",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://skmpublicschool.edu/about-us",
+      url: "https://skmps.org/about-us",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
     },
     {
-      url: "https://skmpublicschool.edu/academics",
+      url: "https://skmps.org/academics",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://skmpublicschool.edu/docs",
+      url: "https://skmps.org/docs",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.7,
     },
     {
-      url: "https://skmpublicschool.edu/fees",
+      url: "https://skmps.org/fees",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.6,
